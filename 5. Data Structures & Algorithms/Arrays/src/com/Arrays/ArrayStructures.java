aea0b882efa0d1dcb773a1dc37d04b04a81e6fe2
vivekworks <vivekts90@gmail.com>
2018-12-27 00:36:50 +0530

    Binary Search complete

--- 5. Data Structures & Algorithms/Arrays/src/com/Arrays/ArrayStructures.java
+++ 5. Data Structures & Algorithms/Arrays/src/com/Arrays/ArrayStructures.java
@@ -1,65 +1,156 @@
 package com.Arrays;
 
 public class ArrayStructures {
-    private int[] theArray= new int[50];
+    private int[] theArray = new int[50];
+    private int[] theArrayOne = new int[50];
+    private int[] theArrayTwo = new int[50];
     private int arraySize = 10;
-    public void generateRandomArray(){
-        for (int i=0;i<arraySize;i++){
-            theArray[i] = (int)(Math.random() * arraySize)+10;
+
+    private void generateRandomArray() {
+        for (int i = 0; i < arraySize; i++) {
+            int val = (int) (Math.random() * arraySize) + 10;
+            theArray[i] = val;
+            theArrayOne[i] = val;
+            theArrayTwo[i] = val;
         }
     }
-    public void printArray(){
-        for (int i=0;i<arraySize;i++){
-            System.out.println("----------");
-            System.out.println("| "+i+" | "+theArray[i]+" |");
-            System.out.println("----------");
-        }
+
+    private void printArray(int[] arrayToPrint) {
+        System.out.println("--------------------------------------------------");
+        for (int i = 0; i < arraySize; i++)
+            System.out.print("|  " + i + " ");
+        System.out.println();
+        System.out.println("--------------------------------------------------");
+        for (int i = 0; i < arraySize; i++)
+            System.out.print("| " + arrayToPrint[i] + " ");
+        System.out.println();
+        System.out.println("--------------------------------------------------");
     }
-    public String getValueAtIndex(int arrayIndex){
-        return arrayIndex>=0 && arrayIndex<arraySize  ? String.valueOf(theArray[arrayIndex]) : "None";
+
+    private String getValueAtIndex(int arrayIndex) {
+        return arrayIndex >= 0 && arrayIndex < arraySize ? String.valueOf(theArray[arrayIndex]) : "None";
     }
-    public boolean doesArrayContainThisValue(int value){
-        boolean result=false;
-        for (int i=0;i<arraySize;i++){
-            if(theArray[i]==value) {
+
+    private boolean doesArrayContainThisValue(int value) {
+        boolean result = false;
+        for (int i = 0; i < arraySize; i++) {
+            if (theArray[i] == value) {
                 result = true;
                 break;
             }
         }
         return result;
     }
-    public String deleteIndex(int arrayIndex){
-        if (arrayIndex<0 && arrayIndex>arraySize) return "Index not found";
-        for (int i=arrayIndex;i<arraySize-1;i++){
-            theArray[i]=theArray[i+1];
+
+    private String deleteIndex(int arrayIndex) {
+        if (arrayIndex < 0 && arrayIndex > arraySize) return "Index not found";
+        for (int i = arrayIndex; i < arraySize - 1; i++) {
+            theArray[i] = theArray[i + 1];
         }
         arraySize--;
         return "Done";
     }
-    public void insertValue(int value){
-        if (arraySize<50) {
+
+    private void insertValue(int value) {
+        if (arraySize < 50) {
             theArray[arraySize] = value;
             arraySize++;
         }
     }
-    public String linearSearchForValue(int value){
-        String result="";
-        for (int i=0;i<arraySize;i++){
+
+    private String linearSearchForValue(int value) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < arraySize; i++) {
             if (theArray[i] == value)
-                result+=" "+i;
+                result.append(" " + i);
         }
-        return result.equals("") ? "-1" : result;
+        return result.toString().equals("") ? "-1" : result.toString();
     }
-    public static void main(String[] args){
+
+    private void swapValues(int firstIndex, int secondIndex, int[] theInputArray) {
+        int temp = theInputArray[firstIndex];
+        theInputArray[firstIndex] = theInputArray[secondIndex];
+        theInputArray[secondIndex] = temp;
+    }
+
+    private void bubbleSortOne() {
+        long startTime = System.currentTimeMillis();
+        int count = 0;
+        int swapCounter = -1;
+        int arraySizeOne = arraySize;
+        while (swapCounter != 0) {
+            swapCounter = 0;
+            for (int i = 0; i < arraySizeOne - 1; i++) {
+                if (theArrayOne[i] > theArrayOne[i + 1]) {
+                    swapValues(i, i + 1, theArrayOne);
+                    swapCounter = 1;
+                }
+                count++;
+                //System.out.println(theArray.toString());
+            }
+            arraySizeOne--;
+            if (arraySizeOne < 1) break;
+            count++;
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Bubble Sort One - TimeTaken : " + (endTime - startTime) + ", Count taken : " + count);
+    }
+
+    private void bubbleSortTwo() {
+        boolean swapped = false;
+        int count = 0;
+        long startTime = System.currentTimeMillis();
+        for (int i = arraySize - 1; i > 1; i--) {
+            for (int j = 0; j < i; j++) {
+                if (theArrayTwo[j] > theArrayTwo[j + 1]) {
+                    swapValues(j, j + 1, theArrayTwo);
+                    swapped = true;
+                }
+                count++;
+                //System.out.println(theArray);
+            }
+            if (!swapped) break;
+            count++;
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Bubble Sort Two - TimeTaken : " + (endTime - startTime) + ", Count taken : " + count);
+    }
+
+    private String binarySearchForValue(int value) {
+        int lowerIndex = 0, higherIndex = arraySize - 1;
+        int foundIndex = -1;
+        while (lowerIndex <= higherIndex) {
+            int middleIndex = (lowerIndex + higherIndex) / 2;
+            if (theArrayOne[middleIndex] == value) {
+                lowerIndex = higherIndex + 1;
+                foundIndex = middleIndex;
+            } else if (theArrayOne[middleIndex] > value)
+                higherIndex = middleIndex - 1;
+            else if (theArrayOne[middleIndex] < value)
+                lowerIndex = middleIndex + 1;
+        }
+        return foundIndex == -1 ? "Not found" : String.valueOf(foundIndex);
+    }
+
+    public static void main(String[] args) {
         ArrayStructures arrayStruct = new ArrayStructures();
         arrayStruct.generateRandomArray();
-        arrayStruct.printArray();
+        System.out.println("Array Generated");
+        arrayStruct.printArray(arrayStruct.theArray);
+        arrayStruct.bubbleSortOne();
         System.out.println(arrayStruct.getValueAtIndex(5));
-        System.out.println("The Array contains the value on the index : "+arrayStruct.doesArrayContainThisValue(12));
+        System.out.println("The Array contains the value on the index : " + arrayStruct.doesArrayContainThisValue(12));
         arrayStruct.deleteIndex(7);
-        arrayStruct.printArray();
+        arrayStruct.printArray(arrayStruct.theArray);
         arrayStruct.insertValue(82);
-        arrayStruct.printArray();
-        System.out.println("The Array contains the value at index : "+arrayStruct.linearSearchForValue(14));
+        arrayStruct.printArray(arrayStruct.theArray);
+        System.out.println("The Array contains the value at index : " + arrayStruct.linearSearchForValue(14));
+        arrayStruct.printArray(arrayStruct.theArrayOne);
+        arrayStruct.bubbleSortOne();
+        arrayStruct.printArray(arrayStruct.theArrayOne);
+        arrayStruct.printArray(arrayStruct.theArrayTwo);
+        arrayStruct.bubbleSortTwo();
+        arrayStruct.printArray(arrayStruct.theArrayTwo);
+        System.out.println(arrayStruct.binarySearchForValue(15));
     }
 }
