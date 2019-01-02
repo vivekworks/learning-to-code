7d3eb8e95b628c00694304966eef4a8c59ed7b9f
vivekworks <vivekts90@gmail.com>
2019-01-03 00:19:18 +0530

    LinkedLists complete

--- 5. Data Structures & Algorithms/LinkedLists/src/com/LinkedLists/TheLinkedList.java
+++ 5. Data Structures & Algorithms/LinkedLists/src/com/LinkedLists/TheLinkedList.java
@@ -3,55 +3,91 @@ package com.LinkedLists;
 public class TheLinkedList {
     private TheLink firstLink;
 
-    private void insertLink(TheLink link){
+    private void insertLink(TheLink link) {
         link.next = firstLink;
         firstLink = link;
     }
 
-    private void removeFirst(){
+    private void removeFirst() {
         firstLink = firstLink.next;
     }
 
-    private TheLink findLink(String bookName){
-        TheLink currentLink  = firstLink;
-        if(firstLink == null)
+    private TheLink findLink(String bookName) {
+        TheLink currentLink = firstLink;
+        if (isEmpty())
             return null;
-        else{
-            while(currentLink.bookName.equals(bookName)){
-                if(currentLink.next != null)
+        else {
+            while (!currentLink.bookName.equals(bookName)) {
+                if (currentLink.next != null)
                     currentLink = currentLink.next;
-                 else
+                else
                     return null;
             }
         }
         return currentLink;
     }
 
-    private void display(){
+    private void removeLink(String bookName) {
         TheLink link = firstLink;
-        if(firstLink == null)
+        TheLink prevLink = null;
+        if (isEmpty())
             System.out.println("List is empty");
-        else{
-            while(link.next != null){
-                System.out.println(link.bookName+", "+link.millionsSold+" million copies sold");
-                link = link.next;
+        else {
+            while (!link.bookName.equals(bookName)) {
+                if (link.next == null) {
+                    System.out.println("Link not found");
+                    return;
+                } else {
+                    prevLink = link;
+                    link = link.next;
+                }
             }
-            System.out.println(link.bookName+", "+link.millionsSold+" million copies sold");
+            if (prevLink == null)
+                firstLink = firstLink.next;
+            else
+                prevLink.next = link.next;
         }
     }
 
-    public static void main(String[] args){
+    private void display() {
+        TheLink link = firstLink;
+        if (firstLink == null)
+            System.out.println("List is empty");
+        else {
+            while (link.next != null) {
+                System.out.println(link.bookName + ", " + link.millionsSold + " million copies sold");
+                link = link.next;
+            }
+            System.out.println(link.bookName + ", " + link.millionsSold + " million copies sold");
+        }
+    }
+
+    private boolean isEmpty() {
+        return firstLink == null;
+    }
+
+    public static void main(String[] args) {
         TheLinkedList linkList = new TheLinkedList();
-        linkList.insertLink(new TheLink("GUT",20));
-        linkList.insertLink(new TheLink("12 RULES FOR LIFE",50));
-        linkList.insertLink(new TheLink("INFERNO",450));
-        linkList.insertLink(new TheLink("DA VINCI CODE",950));
+        linkList.insertLink(new TheLink("GUT", 20));
+        linkList.insertLink(new TheLink("12 RULES FOR LIFE", 50));
+        linkList.insertLink(new TheLink("INFERNO", 450));
+        linkList.insertLink(new TheLink("DA VINCI CODE", 950));
         linkList.display();
+        System.out.println("--------------------------------------------------------");
         linkList.removeFirst();
         linkList.display();
-        linkList.insertLink(new TheLink("COBRA",70));
+        System.out.println("--------------------------------------------------------");
+        linkList.insertLink(new TheLink("COBRA", 70));
         linkList.display();
+        System.out.println("--------------------------------------------------------");
         TheLink linkToFind = linkList.findLink("COBRA");
-        System.out.println(linkToFind == null ? "Not found" : linkToFind.bookName+" - "+linkToFind.millionsSold+" - Next link : "+linkToFind.next.bookName);
+        System.out.println(linkToFind == null ? "Not found" : linkToFind.bookName + " - " + linkToFind.millionsSold + " - Next link : " + linkToFind.next.bookName);
+        System.out.println("--------------------------------------------------------");
+        linkList.removeLink("COBRA");
+        linkList.display();
+        System.out.println("--------------------------------------------------------");
+        linkList.removeLink("12 RULES FOR LIFE");
+        linkList.display();
+        System.out.println("--------------------------------------------------------");
     }
 }
