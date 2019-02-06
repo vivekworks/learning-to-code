8edba85170a2af73a3e99c24f6a6236d732d85f0
vivekworks <vivekts90@gmail.com>
2019-02-06 23:55:52 +0530

    HackerRank problems

---
+++ 6. HackerRank/Algorithms/problems/main/Solution.java
@@ -0,0 +1,10 @@
+package main;
+
+import warmup.MinMaxSum;
+
+public class Solution {
+    public static void main(String[] args){
+        int[] minMaxArray = {256741038, 623958417, 467905213, 714532089, 938071625};
+        MinMaxSum.miniMaxSum(minMaxArray);
+    }
+}
