eac2fa66254ad5fe8bf8eb713a6730d68e2c6998
vivekworks <vivekts90@gmail.com>
2019-02-14 00:27:28 +0530

    Basics revision

--- 5. Data Structures & Algorithms/Basics/src/com/topics/Annotation.java
+++ 5. Data Structures & Algorithms/Basics/src/com/topics/Annotation.java
@@ -2,26 +2,28 @@ package com.topics;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.util.Arrays;
+
+
 
 @Retention(RetentionPolicy.RUNTIME)
-@interface Anno{
-    int val();
-    String post();
-}
 @interface Author{
     String name();
+    String date();
+    String[] reviewers();
+    String modifiedBy();
+    String modifiedDate();
+    String version();
 }
 
 public class Annotation {
-    @Anno(val=7,post="Vaish")
+    @Author(name="Vivek",date="13/02/2019",reviewers = {"Vivek","Arti"},modifiedBy = "Vivek",modifiedDate = "14/02/2019",version = "1.1")
     public void annoCheck(){
         Annotation annotation = new Annotation();
         Class<?> cla = annotation.getClass();
         System.out.println(cla);
         for(var meth :cla.getMethods()){
             System.out.println(meth);
-            System.out.println(meth.getAnnotation(Anno.class)+" - "+meth.getAnnotation(Anno.class).val()+" - "+meth.getAnnotation(Anno.class).post());
+            System.out.println(meth.getAnnotation(Author.class)+" - "+meth.getAnnotation(Author.class).name()+" - "+meth.getAnnotation(Author.class).date());
         }
     }
 }
