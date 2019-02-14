fc9037d308373df134624a4639e0dce86724e311
vivekworks <personalspaceninety@gmail.com>
2019-02-14 21:23:12 +0530

    Basics revision

--- 5. Data Structures & Algorithms/Basics/src/com/topics/Annotation.java
+++ 5. Data Structures & Algorithms/Basics/src/com/topics/Annotation.java
@@ -2,28 +2,28 @@ package com.topics;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-
+import java.util.Arrays;
 
 
 @Retention(RetentionPolicy.RUNTIME)
 @interface Author{
-    String name();
+    String name() default "Vivek";
     String date();
     String[] reviewers();
     String modifiedBy();
     String modifiedDate();
-    String version();
+    double version();
 }
 
-public class Annotation {
-    @Author(name="Vivek",date="13/02/2019",reviewers = {"Vivek","Arti"},modifiedBy = "Vivek",modifiedDate = "14/02/2019",version = "1.1")
-    public void annoCheck(){
+ class Annotation {
+    @Author(date="13/02/2019",reviewers = {"Vivek","Arti"},modifiedBy = "Vivek",modifiedDate = "14/02/2019",version = 1.1)
+     void annoCheck(){
         Annotation annotation = new Annotation();
         Class<?> cla = annotation.getClass();
         System.out.println(cla);
         for(var meth :cla.getMethods()){
             System.out.println(meth);
-            System.out.println(meth.getAnnotation(Author.class)+" - "+meth.getAnnotation(Author.class).name()+" - "+meth.getAnnotation(Author.class).date());
+            System.out.println(meth.getAnnotation(Author.class)+" - "+meth.getAnnotation(Author.class).name()+" - "+meth.getAnnotation(Author.class).date()+" - "+ Arrays.toString(meth.getDeclaredAnnotations()));
         }
     }
 }
