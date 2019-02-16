c85b80395fff4e2431ade5d91d69cc437d64b496
vivekworks <vivekts90@gmail.com>
2019-02-17 00:45:25 +0530

    Basics revision

--- 5. Data Structures & Algorithms/Basics/src/com/io/ReadConsole.java
+++ 5. Data Structures & Algorithms/Basics/src/com/io/ReadConsole.java
@@ -4,16 +4,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class ReadConsole {
-    public void getInput() throws IOException {
+class ReadConsole {
+    void getInput() throws IOException {
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader buffRead = new BufferedReader(isr);
         char c;
         System.out.println("Enter characters, q to quit");
         c = (char) buffRead.read();
+        System.out.println(c);
         do {
             c = (char) buffRead.read();
             System.out.println(c);
         } while (c != '\n');
+        System.out.println("Enter string, q to quit");
+        String s = buffRead.readLine();
+        System.out.println(s);
+        do{
+            s=buffRead.readLine();
+            System.out.println(s);
+        } while (s.equals(String.valueOf('\n')));
     }
 }
