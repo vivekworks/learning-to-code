dab57ece8de4003467f7ba31be53ee87bc9b72b9
vivekworks <vivekts90@gmail.com>
2019-02-17 21:40:53 +0530

    java io over

--- 5. Data Structures & Algorithms/Basics/src/com/io/ConsoleInputMain.java
+++ 5. Data Structures & Algorithms/Basics/src/com/io/ConsoleInputMain.java
@@ -3,16 +3,18 @@ package com.io;
 import java.io.IOException;
 
 public class ConsoleInputMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         try {
             ReadConsole readConsole = new ReadConsole();
             //readConsole.getInput();
             WriteConsole writeConsole = new WriteConsole();
-            writeConsole.showOutput();
+            //writeConsole.showOutput();
             ReadFile readFile = new ReadFile();
             //readFile.readFile();
             WriteFile writeFile=new WriteFile();
             //writeFile.writeFile();
+            ReadAndWrite readAndWrite = new ReadAndWrite();
+            readAndWrite.readWrite();
         } catch (Exception e) {
             throw e;
         }
---
+++ 5. Data Structures & Algorithms/Basics/src/com/io/ReadAndWrite.java
@@ -0,0 +1,38 @@
+package com.io;
+
+import java.io.*;
+import static java.lang.Math.*;
+
+public class ReadAndWrite {
+    boolean flago;
+    ReadAndWrite(){
+        this(true);
+    }
+    ReadAndWrite(boolean flag){
+        this(flag,flag);
+    }
+    ReadAndWrite(boolean flag1,boolean flag2){
+        System.out.println(flag1+" - "+flag2);
+    }
+    void readWrite() throws IOException {
+        String line;
+        String path=System.getProperty("user.dir")+"\\Basics\\src\\com\\io\\";
+        BufferedReader readBuffer = new BufferedReader(new InputStreamReader(System.in));
+        File newFile = new File(path+"write.txt");
+        try(FileOutputStream fos = new FileOutputStream(newFile)) {
+            while (!((line = readBuffer.readLine()).equals("end"))) {
+                for(char c :line.toCharArray())
+                    fos.write(c);
+                fos.write('\n');
+            }
+        }
+        int c;
+        try(FileInputStream fis = new FileInputStream(newFile);PrintWriter writer = new PrintWriter(System.out)){
+            while((c = fis.read()) != -1)
+                writer.print((char)c);
+            writer.flush();
+        }
+        int a=10;
+        double b=pow(10,2);
+    }
+}
