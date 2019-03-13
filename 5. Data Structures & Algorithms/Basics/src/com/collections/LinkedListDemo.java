741060e5fe1d91f9c670c911ce9a7b5ff791e186
vivekworks <vivekts90@gmail.com>
2019-03-13 07:37:39 +0530

    Collections

--- 5. Data Structures & Algorithms/Basics/src/com/collections/LinkedListDemo.java
+++ 5. Data Structures & Algorithms/Basics/src/com/collections/LinkedListDemo.java
@@ -1,6 +1,7 @@
 package com.collections;
 
 import java.util.LinkedList;
+import java.util.Spliterator;
 
 public class LinkedListDemo {
     public static void check(){
@@ -21,5 +22,14 @@ public class LinkedListDemo {
         System.out.println(linkedListDemo);
         //System.out.println(linkedListDemo.get(4));
         System.out.println(linkedListDemo);
+    }
+
+    public static void addressCheck(){
+        LinkedList<Address> addressLinkedList = new LinkedList<>();
+        addressLinkedList.add(new Address("Manhattan","Eula Street","Chicago","Nebraska","54745242"));
+        addressLinkedList.add(new Address("Pasadena","Village Street","California","Newark","55454523"));
+        addressLinkedList.add(new Address("Minnesota","Marshall Street","Missouri","CountryTown","58657432"));
+        Spliterator<Address> addressSpliterator = addressLinkedList.spliterator();
+        addressSpliterator.forEachRemaining((n) -> System.out.println(n));
     }
 }
